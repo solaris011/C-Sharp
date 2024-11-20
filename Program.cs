08d6e2056e20aac156ef97512551c1aca87a3a2f
solaris0250 <solaris0250@gmail.com>
2024-11-19 21:00:00 -0600

    Primer avance

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
+
+string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+
+int x = 0;
+
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+
+string new_message = new String(charMessage);
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
